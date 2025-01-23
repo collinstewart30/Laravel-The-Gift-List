90b9ea4d83538aab297ad111036ec4bda6c3d8a1
collinstewart30 <collin.stewart30@yahoo.com>
2025-01-23 17:44:09 -0500

    Fixed URL prepend http://

--- the-gift-list/app/Http/Controllers/MylistController.php
+++ the-gift-list/app/Http/Controllers/MylistController.php
@@ -86,6 +86,10 @@ class MylistController extends Controller
         //Dont allow to store code - strip tags
         $incomingFields['name'] = strip_tags($incomingFields['name']);
         $incomingFields['item_url'] = strip_tags($incomingFields['item_url']);
+        // Prepend "http://" if the URL doesn't start with a scheme
+        if (!preg_match('/^https?:\/\//', $incomingFields['item_url'])) {
+            $incomingFields['item_url'] = 'http://' . $incomingFields['item_url'];
+        }
         //match "list_id" to the id of the list in mylists table
         $incomingFields['list_id'] = $list->id;
         //Insert fields into DB
